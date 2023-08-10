c8746f546a1564d4ce315ac566c20d065dae7a03
Ifeoma Jonah <adejokejonah@gmail.com>
2023-08-10 14:55:11 -0700

    Task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
-/* A C program that prints a string */
+/* Welcome to pogramming */
 #include <stdio.h>
+
+/**
+ * main - entry point for the program
+ *
+ * puts - outputs a string and appends a new line
+ * Return: return value is 0 to indicate a success
+*/
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/* C Programming */
+#include <stdio.h>
+
+/**
+ * main - entry point for program
+ *
+ * printf - print the string without a new line
+ * Return: returns 0 to indicate success
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
