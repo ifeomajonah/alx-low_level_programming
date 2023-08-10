54958a369060dc8192dc980cc602160600964c8c
Ifeoma Jonah <adejokejonah@gmail.com>
2023-08-10 15:09:33 -0700

    mandatory done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ int main(void)
 	double d;
 	float f;
 
-	printf("Size of a char: %zu byte(s)/n", sizeof(c));
-	printf("Size of an int: %zu byte(s)/n", sizeof(i));
-	printf("Size of a long int: %zu byte(s)/n", sizeof(i));
-	printf("Size of a long long int: %zu byte(s)/n", sizeof(d));
-	printf("Size of a float: %zu byte(s)/n", sizeof(f));
+	printf("Size of a char: %zu byte(s)\n", sizeof(c));
+	printf("Size of an int: %zu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(i));
+	printf("Size of a long long int: %zu byte(s\n", sizeof(d));
+	printf("Size of a float: %zu byte(s)\n", sizeof(f));
 	return (0);
 }
