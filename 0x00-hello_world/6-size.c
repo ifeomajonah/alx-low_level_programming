d79475ac4e9d99958ae54e77895b0f638ed0de1e
Ifeoma Jonah <adejokejonah@gmail.com>
2023-08-10 15:04:27 -0700

    Task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,7 +18,7 @@ int main(void)
 	printf("Size of a char: %zu byte(s)/n", sizeof(c));
 	printf("Size of an int: %zu byte(s)/n", sizeof(i));
 	printf("Size of a long int: %zu byte(s)/n", sizeof(i));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
-	printf("Size of a float: %zu byte(s)\n", sizeof(f));
+	printf("Size of a long long int: %zu byte(s)/n", sizeof(d));
+	printf("Size of a float: %zu byte(s)/n", sizeof(f));
 	return (0);
 }
