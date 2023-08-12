b68d9e8ee877a85b722e2df7dc53d5bbc49ceb2f
Ifeoma Jonah <adejokejonah@gmail.com>
2023-08-11 17:29:51 -0700

    Task

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,11 +4,24 @@
 /**
  * main - entry point
  *
- * Description: function that prints lowercase 
+ * Description: function that prints lowercase
  * excluding some alphabets
  * Return: return 0 for succes
  */
 
 int main(void)
 {
-	char lowercase = 'a'
+	char lowercase = 'a';
+
+	while (lowercase <= 'z')
+	{
+		if (lowercase != 'q' && lowercase != 'e')
+		{
+			putchar(lowercase);
+		}
+		lowercase++;
+	}
+	putchar('\n');
+
+	return (0);
+}
