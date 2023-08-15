ab5f7d02f8cf5434af7939730f5c8f048e482abc
Ifeoma Jonah <adejokejonah@gmail.com>
2023-08-15 15:09:13 -0700

    Test

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,18 +11,24 @@ void jack_bauer(void)
 
 	for (y = 0; y <= 2; y++)
 	{
-		for (m = 0; m <= 3; m++)
+		for (m = 0; m <= 9; m++)
 		{
 			for (z = 0; z <= 5; z++)
 			{
 				for (s = 0; s <= 9; s++)
 				{
-					_putchar(y + '0');
-					_putchar(m + '0');
-					_putchar(':');
-					_putchar(z + '0');
-					_putchar(s + '0');
-					_putchar('\n');
+					if (!( y == 2 && (m >= 4)))
+							{
+								_putchar(y + '0');
+								_putchar(m + '0');
+								_putchar(':');
+								_putchar(z + '0');
+								_putchar(s + '0');
+								_putchar('\n');
+								
+							} else {
+								_putchar(' ');
+							}
 				}
 			}
 		}
