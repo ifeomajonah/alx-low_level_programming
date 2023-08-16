93f7322c01366703f4a49bec08f1010a85017376
Ifeoma Jonah <adejokejonah@gmail.com>
2023-08-16 14:36:48 -0700

    task 9 test

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,21 +15,51 @@ void times_table(void)
 		for (l = 0; l <= 9; l++)
 		{
 			result = i * l;
+			if (l == 9 && result == 0)
+                                {
+                                        _putchar(' ');
+                                        _putchar(result + '0');
+                                }
+
+			  if (l == 9 && result == 9)
+                                {
+                                        _putchar(' ');
+                                        _putchar(result + '0');
+                                }
 			if (result <= 9)
 			{
-				_putchar(result + '0');
+				 if (l == 9 && result == 0 && result == 9)
+                               /* {
+                                        _putchar(' ');
+                                        _putchar(result + '0');
+                                }*/
+
+				if (l == 0 && result == 0)
+				{
+					_putchar(result + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+
+				/*if (l == 9 && result == 0 && result == 9)
+				{
+					_putchar(' ');
+					_putchar(result + '0');
+				}*/
+
+				if(l != 9 && result <= 9)
+				{
+					_putchar(' ');
+					_putchar(result + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 			else if (result > 9)
 			{
 				_putchar((result / 10) + '0');
 				_putchar((result % 10) + '0');
 
-			}
-			if (l != 9 && result <= 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
 			}
 			 if (l != 9 && result > 9)
 			{
