259a0cc42c3012d26f85e96e9ba49a378534be6a
Ifeoma Jonah <adejokejonah@gmail.com>
2023-08-17 09:31:46 -0700

    muml

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,16 +3,13 @@
 /**
  * mul - function that multiplies two integers
  * @a: first integer
- * @d: second integer
+ * @b: second integer
  *
  * Return: return 0 for success
  */
 
 int mul(int a, int b)
 {
-	int result;
-	
-	result = a * b;
 
-	return (result);
+	return (a * b);
 }
