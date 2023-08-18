085e577a2123b834255c43c340c6e3dcafadf0a7
Ifeoma Jonah <adejokejonah@gmail.com>
2023-08-17 17:48:47 -0700

    Message

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * print_number - prints integers
+ * @n: integrer to print
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int i, r;
+
+	if (n >= 0 && n <= 9)
+	{
+		_putchar(n + '0');
+	}
+	else if (n >= -9 && n <= -1)
+	{
+		_putchar('-');
+		i = -n;
+		_putchar(i + '0');
+	}
+	else if (n >= 10 && n <= 99)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n >= -99 && n <= -10)
+	{
+		_putchar('-');
+		r = -n;
+		_putchar((r / 10) + '0');
+		_putchar((r % 10) + '0');
+	}
+	else if (n >= 100 && n < 1000)
+	{
+		_putchar((n / 100) + '0');
+		_putchar(((n % 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	/*else if ( )
+	{
+	} */
+	else if (n >= 1000 && n < 10000)
+	{
+		_putchar((n / 1000) + '0');
+		_putchar(((n / 100) % 10) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+}
