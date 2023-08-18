7bfe931812c1de34ed8c038b08424f47e82eae88
Ifeoma Jonah <adejokejonah@gmail.com>
2023-08-17 21:28:01 -0700

    Test Group

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,50 +9,16 @@
 
 void print_number(int n)
 {
-	int i, r, m;
+	unsigned int k = n;
 
-	if (n >= 0 && n <= 9)
+	if (n < 0)
 	{
-		_putchar(n + '0');
-	}
-	else if (n >= -9 && n <= -1)
-	{
+		n *= -1;
+		k = n;
 		_putchar('-');
-		i = -n;
-		_putchar(i + '0');
 	}
-	else if (n >= 10 && n <= 99)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n >= -99 && n <= -10)
-	{
-		_putchar('-');
-		r = -n;
-		_putchar((r / 10) + '0');
-		_putchar((r % 10) + '0');
-	}
-	else if (n >= 100 && n < 1000)
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n % 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n <= -100 && n > -1000)
-	{
-		_putchar('-');
-		m = -n;
-		_putchar((m / 1000) + '0');
-                _putchar(((m / 100) % 10) + '0');
-                _putchar(((m / 10) % 10) + '0');
-                _putchar((m % 10) + '0');
-	}
-	else if (n >= 1000 && n < 10000)
-	{
-		_putchar((n / 1000) + '0');
-		_putchar(((n / 100) % 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
+	k /= 10;
+	if (k != 0)
+		print_number(k);
+	_putchar((unsigned int) n % 10 + '0');
 }
