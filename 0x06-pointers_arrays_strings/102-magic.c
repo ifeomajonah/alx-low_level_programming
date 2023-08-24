6f77730c127be161f91324a0ad041c53cad1d53e
Ifeoma Jonah <adejokejonah@gmail.com>
2023-08-24 14:19:56 -0700

    102 test

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - entry point
- *
- * Return: 0 for success
- */
-
 int main(void)
 {
 	int n;
@@ -26,5 +20,4 @@ int main(void)
   /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
-}
 }
