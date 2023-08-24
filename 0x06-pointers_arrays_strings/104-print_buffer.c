a5b9b8d698e1705cce959271281c27a6fafc6e18
Ifeoma Jonah <adejokejonah@gmail.com>
2023-08-24 14:32:23 -0700

    test advance

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,41 +11,41 @@
 
 void print_buffer(char *b, int size)
 {
-	int o, j, i;
+	int i, j;
 
-	o = 0;
-
+	i = 0;
 	if (size <= 0)
+		putchar('\n');
+	else
 	{
-		printf("\n");
-		return;
-	}
-	while (o < size)
-	{
-		j = size - o < 10 ? size - o : 10;
-		printf("%08x: ", o);
-		for (i = 0; i < 10; i++)
+		while (i < size)
 		{
-			if (i < j)
-				printf("%02x", *(b + o + i));
-			else
-				printf("  ");
-			if (i % 2)
+			printf("%08x: ", i);
+			j = 0;
+			while (j < 10)
 			{
-				printf(" ");
+				if (j % 2 == 0 && j > 0)
+					printf(" ");
+				if (j + i > size - 1)
+					printf("  ");
+				else
+					printf("%.2x", b[j + i]);
+				j++;
 			}
-		}
-		for (i = 0; i < j; i++)
-		{
-			int c = *(b + o + i);
-
-			if (c < 32 || c > 132)
+			putchar(' ');
+			j = 0;
+			while (j < 10)
 			{
-				c = '.';
+				if (j + i > size - 1)
+					break;
+				if (b[j + i] >= ' ' && b[j + i] <= '~')
+					putchar(b[j + i]);
+				else
+					putchar('.');
+				j++;
 			}
-			printf("%c", c);
+			putchar('\n');
+			i += 10;
 		}
-		printf("\n");
-		o += 10;
 	}
 }
