5bcc01f0b4c7126c2f99e7e3eebec53a90a1b17a
Ifeoma Jonah <adejokejonah@gmail.com>
2023-08-28 15:47:59 -0700

    Task done so far

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,13 +10,15 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack != '\0')
+	int i, j;
+
+	for (i = 0; haystack[i]; i++)
 	{
-		if (*haystack == *needle)
-		{
-			return (haystack);
-		}
-		haystack++;
+		for (j = 0; needle[j] && haystack[i + j]; j++)
+			if (haystack[i + j] != needle[j])
+				break;
+		if (!needle[j])
+			return (haystack + i);
 	}
 	return ('\0');
 }
