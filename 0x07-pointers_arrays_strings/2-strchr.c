966c49884d963ce08f80f4abbc18794f76cd362f
Ifeoma Jonah <adejokejonah@gmail.com>
2023-08-28 10:52:17 -0700

    Fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,16 +12,16 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i])
+	while (s[i] != c && s[i] != '\0')
 	{
+		i++;
+	}
 		if (s[i] == c)
 		{
 			return (&s[i]);
 		}
-
-		i++;
-	}
-	return ('\0');
-
-
+		else
+		{
+			return ('\0');
+		}
 }
