946d12ad43c53a0d2b37b911aa1a8f60416f14af
Ifeoma Jonah <adejokejonah@gmail.com>
2023-08-30 07:57:31 -0700

    Task zero to two done for test

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: integer
+ */
+
+int _strlen_recursion(char *s)
+{
+	static int i = 0;
+
+	if (*s)
+	{
+	       	_strlen_recursion(s + 1);
+		i++;		
+		return (i);
+	}
+	return (0);
+}
