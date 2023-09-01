1e8fe28edb60c09f7aa67c23566def77ad5c99ad
Ifeoma Jonah <adejokejonah@gmail.com>
2023-09-01 12:28:28 -0700

    Commit task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argv: pointer to arguement string
+ * @argc: pointer array size
+ *
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc => 1)
+	{
+		printf("%s", argv[0]);
+	}
+	return (0);
+}
